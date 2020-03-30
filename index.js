e6818cae925aeb73a6404013f98b7d1cb4adc7e1
mmemon21 <mmemon@csumb.edu>
2020-03-30 00:26:40 +0000

    All Files

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+
+/* Configure our server to read public folder and ejs files */
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+/* The handler for the DEFAULT route */
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+/* The handler for undefined routes */
+app.get('*', function(req, res){
+   res.render('error'); 
+});
+
+/* Start the application server */
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Server has been started');
+})
